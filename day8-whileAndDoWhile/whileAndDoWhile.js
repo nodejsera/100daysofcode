04b213e3d9031c82fab2f89823c210a76cdb9939
glarejs <rajatsharma2612@gmail.com>
2019-01-24 22:59:53 +0530

    While and Do while Conditions in node.js

---
+++ day8-whileAndDoWhile/whileAndDoWhile.js
@@ -0,0 +1,9 @@
+var x = 5;
+
+while(x < 5){
+	console.log("Will Not Be executed as the condition is false");
+}
+
+do{
+	console.log("Will Be executed Once even though the condition is false");
+}while(x < 5)
