0b8bf93bc210101fc0628287112ae06f49f0730f
glarejs <rajatsharma2612@gmail.com>
2019-01-30 00:28:10 +0530

    Array sort in node.js

---
+++ day11-arraySort/index.js
@@ -0,0 +1,9 @@
+//Sort method in arrays
+
+var days = ['Monday', 'Tuesday', 'wednesday', 'thursday','friday','saturday','sunday'];
+days.sort();
+console.log(days);
+
+var arr1 = [1,51,50,30,42,5,21];
+arr1.sort();
+console.log(arr1);
